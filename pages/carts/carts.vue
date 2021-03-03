dcb56f73a8504716d6963b8d30cac73cdf11cd81
strangeprogramer <55955324+strangeprogramer@users.noreply.github.com>
2021-03-04 01:25:56 +0800

    Add files via upload

---
+++ pages/carts/carts.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<view class="header" style="padding-top: 20upx;padding-bottom: 20upx;">Shopping cart</view>
+		<view class="operate">
+			<view>
+				<checkbox-group class="flex align-center" @change="changeAll">
+				<checkbox :checked="checked" :class="checked==true?'checked':''" class=" blue round" style="zoom:70%"></checkbox>
+				<text style="font-size: 110%; margin-left: 15upx; color:#747373">Select all</text>
+				</checkbox-group>
+			</view>
+			<view>
+				<view class="cu-btn sm round" style="color: #747373;">Delete</view>
+			</view>
+		</view>
+		
+		<view class="total flex justify-between align-center">
+			<view style="font-size: 110%;color:#747373;">Subtotal <text style="padding-left: 10upx; font-family: monospace; font-size: 130%;"> $600</text></view>
+			<view>
+				<view class="cu-btn bg-gradual-blue round" >Checkout</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				checked:true
+			}
+		},
+		methods: {
+			changeAll(e){
+				this.checked?this.checked=false:this.checked=true;
+			}
+		}
+	}
+</script>
+
+<style>
+	
+.header{
+	text-align: left;
+	font-size: 170%;
+	background-color: white;
+	font-family: monospace;
+	padding-left: 20upx;
+	border-bottom: 1upx solid #e7e7e7
+}
+	
+.operate{
+	position: fixed;
+	background-color: white;
+	display: flex;
+	justify-content: space-between;
+	height: 80upx;
+	align-items: center;
+	width: 750upx;
+	padding-left: 30upx;
+	padding-right: 30upx;
+}
+
+.total{
+	position: fixed;
+	bottom: 100upx;
+	width: 750upx;
+	height: 100upx;
+	background-color: white;
+	padding-left: 30upx;
+	padding-right: 20upx;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,105 @@
+<template>
+	<view class="content">
+		
+		<!--Search bar-->
+		<view class="cu-bar search bg-white">
+			<view class="search-form round">
+				<text class="cuIcon-search"></text>
+				<input v-model="search" placeholder=""/>
+			</view>
+		</view>
+		
+		<!--Tab-->
+		<view>
+			<scroll-view class="nav bg-white" scroll-x>
+				<view class="flex text-center">
+					<view :class="thistab==index?'text-blue cur':''" @click="Switch(index)" v-for="(item,index) in TabLists" :key="index" class="cu-item flex-sub">
+						{{item}}
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+		
+		<!---->
+		<view style="margin-top:0upx;">
+			<view style="padding:20upx 20upx 20upx 5upx;border-bottom: 1upx solid #e7e7e7;"class="flex justify-around bg-white" v-for="(p,i) in goodsLists" :key="i" >
+				<view style="width: 300upx;">
+					<image style="width: 300upx;height: 300upx;" :src="p.url"></image>
+				</view>
+				<view style="width: 400upx; margin-top: 35upx;borflex-direction: column; " >
+					<view style=" font-size: 110%;">
+						{{p.title}}
+					</view>
+					<view style="font-size: 130%;padding-top: 10upx;">
+						<text style="color: #06618c;"><text style="font-size: 85%;">$</text>{{p.price}}</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				search:"",
+				TabLists:["Hot","Fruit","Vegetable","Meat"],
+				thistab:0,
+				goodsList:[],
+			}
+		},
+		onLoad() {
+			this.getgoodsLists();
+		},
+		methods: {
+			Switch(index){
+				this.thistab=index
+			},
+			
+			getgoodsLists(){
+				var data=[];
+				for(var i=0;i<10;i++){
+					var p={
+						id:1,
+						title:'Apple iPhone 11 (128GB) - Purple (SIM-Free)',
+						url:"../../static/icon/71xn9bCRfhL._FMwebp__.webp",
+						price:600,
+						
+					}
+					data.push(p);
+				}
+				this.goodsLists=data;
+			}
+		}
+	}
+</script>
+
+<style>
+	/* .content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	} */
+</style>
